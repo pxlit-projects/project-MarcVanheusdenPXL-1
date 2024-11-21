0fbed103b270245b3f550fb73ab824095864e405
MarcVanheusdenPXL <12001227@student.pxl.be>
2024-11-21 08:07:42 +0100

    Begin setup Back-end and microservices

---
+++ backend-java/post-service/src/main/java/be/pxl/services/domain/Post.java
@@ -0,0 +1,28 @@
+package be.pxl.services.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "post")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private Long authorId;
+    private String title;
+    private String content;
+    private String imageUrl;
+    private String date;
+    private boolean isDraft;
+    private boolean isPublished;
+
+}
